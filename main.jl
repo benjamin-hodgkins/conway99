f78a112e66fca8184856063d5315c474aa0e066c
benjamin-hodgkins <benjamin.a.hodgkins@maine.edu>
2024-05-01 13:58:13 -0400

    Back to trying custom graph generation

--- main.jl
+++ main.jl
@@ -5,6 +5,7 @@ using Random, Combinatorics
 using Graphs, GraphRecipes, Plots
 using BenchmarkTools, Profile
 using CUDA
+using Test
 
 #Returns Adjacency matrix of {9,4,1,2} (Paley 9)
 function paley9()
@@ -75,21 +76,81 @@ function allPermutations(n, k)
         r = set + c # Find the rightmost 1-bit that can be moved left into a 0-bit. Move it left one
         set = (((r ⊻ set) >> 2) ÷ c) | r # take the other bits of the rightmost cluster of 1 bits and place them as far to the right as possible 
         i+=1
-    end
+    end 
     return adj_mat
 end
 
+#TODO https://www.redperegrine.net/2021/04/10/software-algorithms-for-k-combinations/#Another-Numbers-Game
+#TODO https://math.stackexchange.com/questions/1227409/indexing-all-combinations-without-making-list
+#TODO Get this to map to k-combination
 #Returns the k-combination of (n choose k) with the provided rank
 function makeRow(n, k, rank)
-    return CoolLexCombinations(n, k)
+    dualOfZero = n - 1
+
+    # Move to base 0
+    rank -= 1
+    
+    #Calculate the dual (base zero)
+    dual = binomial(n, k) - 1 - rank
+    
+    #Gets combinadic of dual
+    combination = combinadic(n, k, dual)
+
+    i = 1
+    while i <= k
+        #Map to zero-based combination
+        combination[i] = dualOfZero - combination[i]
+
+        #Add 2 (for base 2)
+        combination[i] += 1
+        i += 1
+    end
+
+    return combination
+end
+
+#Calculates zero-based array of c such that maxRank = (c1 choose k-1) + (c2 choose k-2) + ... (c[of k-1] choose 1)
+function combinadic(n, k, maxRank)
+    result = Array{Int}(undef, k)
+    diminishingRank = maxRank
+    reducingK = k
+
+    i = 1
+    while i <= k #TODO Debug, returning wrong values for [9,4]
+        result[i] = largestValue(n, reducingK, diminishingRank)   
+        diminishingRank -= binomial(result[i], reducingK)
+        reducingK -= 1
+        i += 1
+    end
+    return result
+end
+
+#Returns the highest rank of n2 choose k2 that is less than the threshold
+function largestValue(n2, k2, threshold)
+    v = n2 - 1   
+    while binomial(v,k2) > threshold
+      v -= 1
+    end
+    return v
+end
+
+function binomialCheck(row)
+    result = 0
+    counter = 0
+    len = length(row)
+    for i in range(1, len)
+        result += binomial(row[i], len - counter)
+        counter += 1
+    end
+    return result
 end
 
 function bruteForce(vertices, degree, start, finish)
     #Multithreading
     Threads.@threads for i in range(start, finish)
-        g = random_regular_graph(vertices, degree, seed=i) #TODO Bottleneck
+        g = random_regular_graph(vertices, degree, seed=i)
         if check(g, vertices) == true
-            fName = "Winner! Seed - " * string(i) * (".lgz")
+            fName = "Winner(1)! Seed - " * string(i) * (".lgz")
             savegraph(fName, g)
             return true
         end 
@@ -98,11 +159,12 @@ function bruteForce(vertices, degree, start, finish)
 end
 
 function bruteForce2(vertices, degree, start, finish)
+    
     #Multithreading
     Threads.@threads for i in range(start, finish)
         g = generateGraph(vertices, degree, i)
         if check2(g) == true
-            fName = "Winner! Seed - " * string(i) * (".lgz")
+            fName = "Winner(2)! Seed - " * string(i) * (".lgz")
             savegraph(fName, g)
             return true
         end 
@@ -133,9 +195,19 @@ function main()
 
     #Compare graph generation 
     #@btime random_regular_graph($V, $D)
-    x = [0,0,0,0,0,1,1,1,1]
-    @time nthperm(x, 1)
-    #@time allPermutations(n, k)
+    #TODO Make first half of graph, insert middleRow, copy first half and reverse for last half
+    middleRow = [Int8(0)]
+    append!(middleRow, repeat([1,0], Int((n-1)/2)))
+
+    row = makeRow(n, k, rank)
+    combinations = allPermutations(n, k)
+    actual = binomialCheck(row)
+
+    target = combinations[rank]
+    println("Row: " * string(row))
+    #println("Combinations: " * string(combinations))
+    println("Target: " * string(target))
+    @test target == actual
     #makeRow(n, k , rank)
 end
 main()

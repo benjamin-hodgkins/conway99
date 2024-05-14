3c7699ce8e3ea3461786db40819a24eede452342
benjamin-hodgkins <benjamin.a.hodgkins@gmail.com>
2024-05-14 14:34:27 -0400

    Working on nextRow computation

--- main.jl
+++ main.jl
@@ -10,25 +10,25 @@ using Test
 
 #Returns Adjacency matrix of {9,4,1,2} (Paley 9)
 function paley9()
-    
-    adj_mat = Array([
-    0 1 1 1 0 0 1 0 0;
-    1 0 0 0 1 0 1 1 0;
-    1 0 0 1 0 0 0 1 1;
-    1 0 1 0 1 1 0 0 0;
-    0 1 0 1 0 1 0 1 0;
-    0 0 0 1 1 0 1 0 1;
-    1 1 0 0 0 1 0 0 1;
-    0 1 1 0 1 0 0 0 1;
-    0 0 1 0 0 1 1 1 0
+
+    adj_mat = Array{Int8}([
+        0 1 1 1 0 0 1 0 0;
+        1 0 0 0 1 0 1 1 0;
+        1 0 0 1 0 0 0 1 1;
+        1 0 1 0 1 1 0 0 0;
+        0 1 0 1 0 1 0 1 0;
+        0 0 0 1 1 0 1 0 1;
+        1 1 0 0 0 1 0 0 1;
+        0 1 1 0 1 0 0 0 1;
+        0 0 1 0 0 1 1 1 0
     ])
     return adj_mat
 end
 
 #Checks properties of graph v1 (work with Julia graph type)
 function check(graph, vertices)
-    for i in range(1,vertices)
-        for j in range(i+1,vertices)
+    for i in range(1, vertices)
+        for j in range(i + 1, vertices)
             if i == j
                 continue
             end
@@ -58,16 +58,16 @@ function check2(adj_mat)
             return false
         end
     end
-    return true   
+    return true
 end
 
 #Generates all permutations of bitstrings of length n with k bits flipped in order
 #You are not expected to understand this
-function allPermutations(n::Int128, k::Int128)
+function allPermutations(n, k)
     # https://programmingforinsomniacs.blogspot.com/2018/03/gospers-hack-explained.html
     # https://iamkate.com/code/hakmem-item-175/
-    set::Int128 = 2^k - 1 
-    limit::Int128 = 2^n 
+    set::Int128 = 2^k - 1
+    limit::Int128 = 2^n
     i::Int128 = 1
     while (set < limit)
         valid = checkPermutation(set, n)
@@ -75,11 +75,11 @@ function allPermutations(n::Int128, k::Int128)
             return set
         end
         #Gosper's hack:
-        c = set & - set # c is equal to the rightmost 1-bit in set.
+        c = set & -set # c is equal to the rightmost 1-bit in set.
         r = set + c # Find the rightmost 1-bit that can be moved left into a 0-bit. Move it left one
         set = (((r ⊻ set) >> 2) ÷ c) | r # take the other bits of the rightmost cluster of 1 bits and place them as far to the right as possible 
-        i+=1
-    end 
+        i += 1
+    end
 end
 
 #Gets locations of flipped bits in b
@@ -87,7 +87,7 @@ function bitLocations(b::Integer)
     flippedBits = ones(Int, count_ones(b))
     j = 1
     bStr = digits(b, base=2)
-    for i in range(1,length(bStr))
+    for i in range(1, length(bStr))
         if bStr[i] == 1
             flippedBits[j] = i
             j += 1
@@ -99,7 +99,7 @@ end
 function checkPermutation(set, n)
     #TODO Check each permutation of first row and following rows
     bits = bitLocations(set)
-    firstRow = reverse(digits(set, base=2, pad=n))
+    firstRow = reverse(digits(Int8, set, base=2, pad=n))
 
     #If there is a loop, return 
     if n in bits
@@ -108,22 +108,21 @@ function checkPermutation(set, n)
 
     #Set length to the first half of the 2d array, minus the middle row if applicable
     length::Int = 0
-    if n % 2 == 0
-        length::Int = n / 2  
+    if n % 2 == 0 
+        length = n / 2
     else
-        length::Int = ((n-1) / 2) - 1
+        length = (n - 1) / 2
     end
 
-    # TODO make sure length is Int
-    adj_mat = Vector{Int8}(undef, length)
-    adj_mat[1:n] = firstRow
-
-    for i::Int in length
+    adj_mat = zeros(Int8, length, n)
+    adj_mat[1, :] = firstRow
+    
+    for i::Int in length #TODO Skip first row
         #TODO pick next row based on bitmasks
-        nextRow = 
-        adj_mat[i*n:i*n+n] = nextRow
+        nextRow = digits(Int8, set, base=2, pad=n)
+        adj_mat[i,:] = nextRow
     end
-
+    display(adj_mat)
     return true
 end
 
@@ -135,13 +134,13 @@ function bruteForce(vertices, degree, start, finish)
             fName = "Winner(1)! Seed - " * string(i) * (".lgz")
             savegraph(fName, g)
             return true
-        end 
+        end
     end
-    return false 
+    return false
 end
 
 function bruteForce2(vertices, degree, start, finish)
-    
+
     #Multithreading
     Threads.@threads for i in range(start, finish)
         g = generateGraph(vertices, degree, i)
@@ -149,9 +148,9 @@ function bruteForce2(vertices, degree, start, finish)
             fName = "Winner(2)! Seed - " * string(i) * (".lgz")
             savegraph(fName, g)
             return true
-        end 
+        end
     end
-    return false 
+    return false
 end
 
 #The graph should have 99 vertices
@@ -161,13 +160,13 @@ end
 function main()
     #TODO https://jenni-westoby.github.io/Julia_GPU_examples/dev/Vector_addition/
     paley = paley9()
-    n::Int128 = 5
-    k::Int128 = 2
-    start  = 1#50000000
+    n = 5
+    k = 2
+    start = 1#50000000
     finish = 200
     #Graph to pass to GPU (use CuArray in main)
     #graph = CuArray{Int}(undef, (degree + 2) * vertices)
-    
+
     #Compare brute force methods
     #@btime bruteForce($V, $D, $start, $finish)
     #@btime bruteForce2($V, $D, $start, $finish)
@@ -178,10 +177,11 @@ function main()
     #@btime random_regular_graph($V, $D)
 
     #TODO Make first half of graph, insert middleRow, copy first half and reverse for last half
-    #middleRow = [Int8(0)]
-    #append!(middleRow, repeat([1,0], Int((n-1)/2)))
+    middleRow = [Int8(0)]
+    append!(middleRow, repeat([1, 0], Int((n - 1) / 2))) #TODO Wrong
 
     #@btime allPermutations($n,$k)
     allPermutations(n, k)
+
 end
 main()
